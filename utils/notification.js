de37944c479a06802371bea05b43c2b4af7cc451
dev03lkomavyax <dev03lko@mavyax.com>
2024-07-26 19:32:04 +0530

    code updated

---
+++ utils/notification.js
@@ -0,0 +1,6 @@
+const notifyDeliveryExec = (deliveryExecId, orderId) => {
+    // Implement your notification logic here
+    console.log(`Notifying delivery executive ${deliveryExecId} about order ${orderId}`);
+};
+
+module.exports = { notifyDeliveryExec }
